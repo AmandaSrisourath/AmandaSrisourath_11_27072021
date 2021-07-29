6a0cb14544c9fa19e37e0bc6da845ec9461c4b62
Amandozz <amanda.srisou@gmail.com>
2021-07-29 16:43:47 +1000

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,21 +5,18 @@ import logo from "../Assets/logo.png";
 class Header extends React.Component {
     render() {
         const {currentPage} = this.props;
-        // const {name} = this.props;
         return (
             <div id={"header"}>
                 <img src={logo} />
 
                 <ul>
                     <li>
-                        <Link id={"home"} className={ currentPage === "home" && "underline" } to="/">Accueil</Link>
+                        <Link id="home" className={ currentPage === "home" && "underline" } to="/">Accueil</Link>
                     </li>
                     <li>
-                        <Link id={"about"} className={ currentPage === "about" && "underline" } to="/about">A propos</Link>
+                        <Link id="about" className={ currentPage === "about" && "underline" } to="/about">A propos</Link>
                     </li>
                 </ul>
-
-                {/*<p>{ name }</p>*/}
             </div>
         );
     }
