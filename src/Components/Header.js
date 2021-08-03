ae873b69ad3dbd011231ca8729bad436f2d70d99
Amandozz <amanda.srisou@gmail.com>
2021-08-03 16:08:09 +1000

    add function

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,18 +3,25 @@ import {Link} from "react-router-dom";
 import logo from "../Assets/logo.png";
 
 class Header extends React.Component {
-    render() {
+    getLinkClass(linkName) {
         const {currentPage} = this.props;
+        if(currentPage === linkName) {
+            return "underline";
+        }
+        return null;
+    }
+
+    render() {
         return (
             <div id={"header"}>
                 <img src={logo} alt="logo-kasa" />
 
                 <ul>
                     <li>
-                        <Link id="home" className={currentPage === "home" && "underline"} to="/">Accueil</Link>
+                        <Link id="home" className={this.getLinkClass("home")} to="/">Accueil</Link>
                     </li>
                     <li>
-                        <Link id="about" className={currentPage === "about" && "underline"} to="/about">A propos</Link>
+                        <Link id="about" className={this.getLinkClass("about")} to="/about">A propos</Link>
                     </li>
                 </ul>
             </div>
