038f0696d60931e8973433d8ae7e31f7fc30e943
Amandozz <amanda.srisou@gmail.com>
2021-08-10 17:07:41 +1000

    add slider

--- src/Pages/Location/Gallery.js
+++ src/Pages/Location/Gallery.js
@@ -3,23 +3,32 @@ import arrowPrevious from "../../Assets/arrow-previous.png";
 import arrowNext from "../../Assets/arrow-next.png";
 
 class Gallery extends React.Component {
-    componentDidMount() {
-        const {pictures} = this.props;
-        console.log(pictures);
-    }
-
     constructor(props) {
         super(props);
         this.state = {pictures:[], currentIndex:0};
     }
 
+    previousClick = () => {
+        this.setState({
+            currentIndex: this.state.currentIndex === 0 ? this.props.pictures.length -1 : this.state.currentIndex -1,
+        });
+    }
+
+    nextClick = () => {
+        this.setState({
+            currentIndex: this.state.currentIndex === this.props.pictures.length -1 ? 0 : this.state.currentIndex +1,
+        });
+    }
+
     render() {
+        console.log(this.state.currentIndex);
         const {pictures = []} = this.props;
         return (
             <div>
-                <img id="location-banner" className="banner" src={pictures[this.state.currentIndex]} alt="location-banner"  />
-                <img className="arrow-previous prev-next" src={arrowPrevious}/>
-                <img className="arrow-next prev-next" src={arrowNext}/>
+                <img id="location-banner" className="banner" src={pictures[this.state.currentIndex]} alt="location-banner" />
+                <img className="arrow-previous" onClick={this.previousClick} src={pictures.length > 1 && arrowPrevious} />
+                <img className="arrow-next" onClick={this.nextClick} src={pictures.length > 1 && arrowNext} />
+                <p className="carrousel-text">{this.state.currentIndex +1}/{pictures.length} </p>
             </div>
         )
     }
